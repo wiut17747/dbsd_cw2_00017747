276606fabb782802019dcb5eb6f6f4d40461f55c
= <jabborovz2005@gmail.com>
2025-03-30 16:08:35 +0500

    feat: sorting, filtering, pagination and export xml added

---
+++ Controllers/BookLoanController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace dbsd_cw2_00017747.Controllers
+{
+    public class BookLoanController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
